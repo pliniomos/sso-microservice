13935c96eac7564ee64265fee0fd5b33e355a3d7
Silva, Plinio <plinio.silva@accenture.com>
2019-09-18 23:39:31 -0300

    .

--- src/main/java/com/pitang/desafio/mv/util/ExceptionMessages.java
+++ src/main/java/com/pitang/desafio/mv/util/ExceptionMessages.java
@@ -4,9 +4,9 @@ public class ExceptionMessages {
 	
 	private ExceptionMessages(){}
 
-	public static final String EXCEPTION_MSG_INVALID_LOGIN 	= "Invalid Login or Password";
-	public static final String EXCEPTION_MSG_UNAUTHORIZED 	= "Unauthorized";
-	public static final String EXCEPTION_MSG_INVALID_TOKEN 	= "Unauthorized - Invalid token";
-	public static final String EXCEPTION_MSG_EXPIRED_TOKEN 	= "Unauthorized - Invalid session";
+	public static final String EXCEPTION_MSG_INVALID_LOGIN 	= "Login ou Senha Inválidos.";
+	public static final String EXCEPTION_MSG_UNAUTHORIZED 	= "Não Autorizado";
+	public static final String EXCEPTION_MSG_INVALID_TOKEN 	= "Não Autorizado - Token Inválido";
+	public static final String EXCEPTION_MSG_EXPIRED_TOKEN 	= "Não Autorizado - Sessão Inválida";
 
 }
